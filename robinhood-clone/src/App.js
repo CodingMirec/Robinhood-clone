b300a4062a9c4eaa812bfefe3a1b106df94cec98
CodingMirec <m.sykora01@gmail.com>
2020-12-21 23:07:48 +0100

    well, first error, css not respinding

--- robinhood-clone/src/App.js
+++ robinhood-clone/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <div className="app__header"></div>
+      <div className="app__header">
+        <Header />
+      </div>
       <div className="app__body"></div>
     </div>
   );
--- robinhood-clone/src/Header.js
+++ robinhood-clone/src/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Header() {
-  return <div className="header"></div>;
+  return (
+    <div className="header__wrapper">
+      <div className="header__logo">{/* logo */}logo</div>
+    </div>
+  );
 }
 
 export default Header;
