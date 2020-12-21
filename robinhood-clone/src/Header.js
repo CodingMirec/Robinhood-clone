f83367d1a8ecd59f491ca6d41259782f89a9d995
CodingMirec <m.sykora01@gmail.com>
2020-12-22 00:22:19 +0100

    header - navbar structure

--- robinhood-clone/src/Header.js
+++ robinhood-clone/src/Header.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Logo from "./robinhood__logo.svg";
 
+import SearchIcon from "@material-ui/icons/Search";
+
+import "./Header.css";
+
 function Header() {
   return (
     <div className="header__wrapper">
       <div className="header__logo">
         <img src={Logo} width={25} />
+      </div>
+      <div className="header__search">
+        <div className="header__searchContainer">
+          <SearchIcon />
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+      <div className="header__menuItems">
+        <a href="#">Free Stocks</a>
+        <a href="#">Portfolio</a>
+        <a href="#">Cash</a>
+        <a href="#">Messages</a>
+        <a href="#">Account</a>
       </div>
     </div>
   );
