dd84e7a7debe31c167146d9bf960733ebfb1e7fc
CodingMirec <m.sykora01@gmail.com>
2020-12-23 00:58:04 +0100

    stocks list, data

--- robinhood-clone/src/Stats.js
+++ robinhood-clone/src/Stats.js
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "./Stats.css";
 
+const TOKEN = "bvh5hvv48v6p4qlb7330";
+const BASE_URL = "https://finnhub.io/api/v1/quote";
+
 function Stats() {
+  const [stockData, setStockData] = useState([]);
+
+  const getStocksData = (stock) => {
+    return axios
+      .get(`${BASE_URL}?symbol=${stock}&token=${TOKEN}`)
+      .catch((error) => {
+        console.error("Error", error.message);
+      });
+  };
+
+  useEffect(() => {
+    let tempStocksData = [];
+    const stocksList = [
+      "AAPL",
+      "MSFT",
+      "TSLA",
+      "FB",
+      "BABA",
+      "UBER",
+      "DIS",
+      "SBUX",
+    ];
+
+    let promises = [];
+    stocksList.map((stock) => {
+      promises.push(
+        getStocksData(stock).then((res) => {
+          console.log(res);
+          tempStocksData.push({
+            name: stock,
+            ...res.data,
+          });
+        })
+      );
+    });
+
+    Promise.all(promises).then(() => {
+      console.log(tempStocksData);
+      setStockData(tempStocksData);
+    });
+  }, []);
+
   return (
     <div className="stats">
       <div className="stats__container">
@@ -10,7 +56,17 @@ function Stats() {
           <p>Stocks</p>
         </div>
         <div className="stats__content">
-          <div className="stats__rows">{/* current stocks */}</div>
+          <div className="stats__rows">
+            {myStocks.map((stock) => (
+              <StatsRow
+                key={stock.data.ticker}
+                name={stock.data.ticker}
+                openPrice={stock.info.o}
+                volume={stock.data.share}
+                price={stock.info.c}
+              />
+            ))}
+          </div>
         </div>
         <div className="stats__header">
           <p>Lists</p>
