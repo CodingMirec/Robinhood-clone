1b0a38b15b56086ee1a15b6bd415a48de2ea1742
CodingMirec <m.sykora01@gmail.com>
2020-12-23 23:55:37 +0100

    pointer / stock chart svg

--- robinhood-clone/src/StatsRow.js
+++ robinhood-clone/src/StatsRow.js
@@ -1,19 +1,20 @@
 import React from "react";
+import StockSVG from "./stock.svg";
 
 import "./StatsRow.css";
 
-function StatsRow() {
+function StatsRow(props) {
   return (
     <div className="row">
       <div className="row__intro">
-        <h1>AAPL</h1>
-        <p>200 shares</p>
+        <h1>{props.name}</h1>
+        <p>{props.shares && props.shares + " shares"}</p>
       </div>
       <div className="row__chart">
-        {/* <img src={stockChart} height={16} /> */}
+        <img src={StockSVG} height={16} />
       </div>
       <div className="row__numbers">
-        <p className="row__price">$200</p>
+        <p className="row__price">{props.price}</p>
         <p className="row__percentage">+200%</p>
       </div>
     </div>
